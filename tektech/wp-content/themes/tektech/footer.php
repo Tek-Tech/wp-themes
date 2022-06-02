ac546703c81b33dead4c16efc840d7525cc97714
dev0ps221 <seybaxscorpion@gmail.com>
2022-06-02 15:11:52 +0000

    tektech

--- tektech/wp-content/themes/tektech/footer.php
+++ tektech/wp-content/themes/tektech/footer.php
@@ -1,6 +1,9 @@
 
     </div>
     <div class='container'>
+        <nav class="navigation-menu">
+            <?php wp_nav_menu() ?>
+        </nav>
         <footer class="site-footer"  <?php if ( get_header_image() ) : ?> style='background-size:100% 100%;min-height:96vh;background-image:url(<?php header_image() ?>);' <?php endif; ?>>
             <p><?php bloginfo( 'name' ) ?></p>
         </footer>
